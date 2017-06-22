78cfa0870879d5be88b6bbb8c41c8bf29ed58bf4
Folusho Oladipo <folusho.oladipo@andela.com>
2017-06-22 13:28:21 +0100

    add section for specifying whether or not a prooduct is digital

--- imports/plugins/included/product-detail-simple/client/components/variantList.js
+++ imports/plugins/included/product-detail-simple/client/components/variantList.js
@@ -3,8 +3,22 @@ import Variant from "./variant";
 import { EditContainer } from "/imports/plugins/core/ui/client/containers";
 import { Divider, IconButton } from "/imports/plugins/core/ui/client/components";
 import { ChildVariant } from "./";
+import variantListStyle from "./variantList.css";
 
 class VariantList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isDigital: props.isDigital || false
+    };
+
+    this.isValidUrl = this.isValidUrl.bind(this);
+    this.updateDownloadUrl = this.updateDownloadUrl.bind(this);
+    this.showProductDownloadUrlInput = this.showProductDownloadUrlInput.bind(this);
+    this.toggleDigitalProduct = this.toggleDigitalProduct.bind(this);
+    this.showDigitalProductToggler = this.showDigitalProductToggler.bind(this);
+  }
 
   handleVariantEditClick = (event, editButtonProps) => {
     if (this.props.onEditVariant) {
@@ -39,6 +53,100 @@ class VariantList extends Component {
     }
 
     return false;
+  }
+
+  isValidUrl(testUrl) {
+    if (/(\.){2,}/.test(testUrl)) {
+      return [false];
+    }
+    if (/(\.)$/.test(testUrl)) {
+      return [false];
+    }
+
+    const trimmedUrl = $.trim(testUrl);
+    /*
+      Regex used is courtesy of Diego Perrini. Link is:
+      https://gist.github.com/dperini/729294
+
+      To know why this test was chosen, see this comparison table:
+      https://mathiasbynens.be/demo/url-regex
+    */
+    const correctUrlPattern = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+    const isUrlValid = correctUrlPattern.test(trimmedUrl);
+    if (!isUrlValid) {
+      return [false];
+    }
+
+    return [isUrlValid, trimmedUrl];
+  }
+
+  updateDownloadUrl(e) {
+    const target = e.target;
+    const targetElement = $(target);
+    const validityResults = this.isValidUrl(target.value);
+    const isUrlValid = validityResults[0];
+    if (!isUrlValid) {
+      targetElement.removeClass("validUrl");
+      targetElement.addClass("invalidUrl");
+      return;
+    }
+    targetElement.removeClass("invalidUrl");
+    targetElement.addClass("validUrl");
+    const validUrl = validityResults[1];
+    this.props.onProductFieldChange(
+      this.props.product._id,
+      "digitalDownloadLink",
+      validUrl
+    );
+  }
+
+  showProductDownloadUrlInput() {
+    if (!this.state.isDigital) {
+      return null;
+    }
+
+    return (
+      <div className="rui textfield form-group">
+        <input
+          id="productDownloadUrl"
+          type="text"
+          className="textfield"
+          placeholder={this.props.product.digitalDownloadLink || "Download link..."}
+          onChange={this.updateDownloadUrl}
+        />
+      </div>
+    );
+  }
+
+  toggleDigitalProduct(e) {
+    const isDigitalProduct = e.target.checked;
+    this.setState({ isDigital: isDigitalProduct });
+    this.props.onProductFieldChange(
+      this.props.product._id,
+      "isDigital",
+      isDigitalProduct
+    );
+  }
+
+  showDigitalProductToggler() {
+    if (this.props.hasAdminPermission) {
+      return (
+        <div className="rui form-group">
+          <label>Digital product?
+          </label>
+          <input
+            id="digital"
+            className="checkbox-switch"
+            type="checkbox"
+            defaultChecked={this.state.isDigital}
+            onChange={this.toggleDigitalProduct}
+          />
+          {this.showProductDownloadUrlInput()}
+        </div>
+      );
+    }
+
+    return null;
   }
 
   renderVariants() {
@@ -95,6 +203,7 @@ class VariantList extends Component {
 
     const variantList = (
       <ul className="variant-list list-unstyled" id="variant-list" key="variantList">
+        {this.showDigitalProductToggler()}
         {variants}
         {addButton}
       </ul>
@@ -177,21 +286,23 @@ class VariantList extends Component {
     return (
       <div className="product-variants">
         {this.renderVariants()}
-        {this.renderChildVariants()}
       </div>
     );
   }
 }
 
 VariantList.propTypes = {
+  _id: PropTypes.string,
   childVariantMedia: PropTypes.arrayOf(PropTypes.any),
   childVariants: PropTypes.arrayOf(PropTypes.object),
   displayPrice: PropTypes.func,
   editable: PropTypes.bool,
+  isDigital: PropTypes.bool,
   isSoldOut: PropTypes.func,
   onCreateVariant: PropTypes.func,
   onEditVariant: PropTypes.func,
   onMoveVariant: PropTypes.func,
+  onProductFieldChange: PropTypes.func,
   onVariantClick: PropTypes.func,
   onVariantVisibiltyToggle: PropTypes.func,
   variantIsSelected: PropTypes.func,
