840f785dd69c30c56fb3cab89be48912df4dfe3a
andela-ieyo <ifiokabasi.eyo@andela.com>
2017-06-22 11:27:29 +0100

    refactor code for restful endpoints and fix bug for add Products (#20)

--- server/startup/rest-endpoints.js
+++ server/startup/rest-endpoints.js
@@ -1,7 +1,12 @@
-import { Accounts, Cart, Emails, Inventory, Orders, Products, Shipping, Shops } from "/lib/collections";
+import { Shops, Products, Orders, Cart, Shipping } from "/lib/collections";
+import Reaction  from "/server/api/core";
 
-export default function () {
-	// Global API configuration
+const hasPermission = (user, role) => {
+  return user.roles[Reaction.getShopId()].includes(role);
+};
+
+export default () => {
+  // Global API configuration
   const Api = new Restivus({
     useDefaultAuth: true,
     prettyJson: true,
@@ -11,96 +16,106 @@ export default function () {
     version: "v1"
   });
 
-  function getApiOptions(collectionName) {
+  const getApiOptions = (collectionName) => {
     return {
       routeOptions: {
         authRequired: true
       },
+
       endpoints: {
-
-		// GET all items in collection
+        // GET all items in collection
         get: {
-          roleRequired: ["owner", "admin"],
-          action: function () {
-            const allRecords = collectionName.find();
-            if (allRecords) {
-              return {
-                statusCode: 201,
-                status: "Succes: All records retreived",
-                data: allRecords
-              };
+          action() {
+            if (hasPermission(this.user, "admin") ||
+            hasPermission(this.user, "guest") ||
+            hasPermission(this.user, "owner")) {
+              const allRecords = collectionName.findOne(this.urlParams.id);
+              if (!allRecords) {
+                return { status: "fail",
+                  message: "An error occurred. Record does not exist" };
+              }
+              return { statusCode: 200, status: "success", data: allRecords };
             }
-            return {
-              statusCode: 404,
-              status: "fail",
-              message: "error"
-            };
           }
         },
 
-		// POST into a collection
+        // POST into a collection
         post: {
-          roleRequired: ["owner", "admin"],
-          action: function () {
-            const isInserted = collectionName.insert(this.bodyParams);
-            if (isInserted) {
-              return {
-                statusCode: 201,
-                status: "success",
-                data: isInserted
-              };
+          action() {
+            if (!(hasPermission(this.user, "admin") ||
+            hasPermission(this.user, "owner"))) {
+              return { statusCode: 403, status: "fail",
+                message: "You do not have permission to add a record" };
             }
-
-            return { status: "fail", message: "error" };
+            if (hasPermission(this.user, "admin") ||
+            hasPermission(this.user, "owner")) {
+              const isInserted = collectionName.insert(this.bodyParams);
+              if (isInserted) {
+                return { statusCode: 201, status: "success", data: isInserted };
+              } return { statusCode: 400, status: "fail",
+                message: "An error occurred. Post was not successful" };
+            }
           }
         },
 
-		// UPDATE a collection
-        patch: {
-          roleRequired: ["owner", "admin"],
-          action: function () {
-            const isUpdated = collection.update(this.urlParams.id, {
-              $set: this.bodyParams
-            });
-            if (isUpdated) {
-              return {
-                statusCode: 201,
-                status: "success",
-                data: isUpdated
-              };
+        // UPDATE a collection
+        put: {
+          action() {
+            if (!(hasPermission(this.user, "admin") ||
+            hasPermission(this.user, "owner"))) {
+              return { statusCode: 403, status: "fail",
+                message: "You do not have permission to edit this record" };
             }
-            return {
-              status: "fail",
-              message: "record not found"
-            };
+            if (hasPermission(this.user, "admin") ||
+            hasPermission(this.user, "owner")) {
+              const isUpdated = collectionName.upsert({ _id: this.urlParams.id }, {
+                $set: this.bodyParams
+              });
+              if (!isUpdated) {
+                return { status: "fail", statusCode: 404,
+                  message: "An error occurred. Record does not exist" };
+              }
+              const record = collectionName.findOne(this.urlParams.id);
+              return { statusCode: 200, status: "success", data: isUpdated, record };
+            }
           }
         },
 
-		// DELETE an record in a collection
+        // DELETE a record in a collection
         delete: {
-          roleRequired: ["owner", "admin"],
-          action: function () {
-            const isDeleted = collection.remove(this.urlParams.id);
-
-            if (isDeleted) {
-              return { status: "success",
-                data: {
-                  message: "record deleted"
-                }
-              };
+          action() {
+            if (!(hasPermission(this.user, "admin") ||
+            hasPermission(this.user, "owner"))) {
+              return { statusCode: 403, status: "fail",
+                message: "You do not have permission to delete this record" };
+            }
+            if (hasPermission(this.user, "admin") ||
+            hasPermission(this.user, "owner")) {
+              // we delete a product by setting the isDeleted flag
+              if (collectionName._name === "Products") {
+                // get collection from db
+                const collection = collectionName.findOne(this.urlParams.id);
+                // modify isDeleted flag
+                collection.isDeleted = true;
+                // update collection in db
+                const isDeleted = collectionName.upsert({ _id: this.urlParams.id }, {
+                  $set: collection
+                });
+                return { data: isDeleted, message: "product has been archived" };
+                // other collections may be removed
+              }
+              const isDeleted = collectionName.remove({ _id: this.urlParams.id });
+              return { status: "success", data: isDeleted,  message: "record is deleted" };
             }
           }
         }
       }
     };
-  }
+  };
 
-  Api.addCollection(Emails, getApiOptions(Emails));
   Api.addCollection(Shops, getApiOptions(Shops));
   Api.addCollection(Products, getApiOptions(Products));
   Api.addCollection(Orders, getApiOptions(Orders));
   Api.addCollection(Cart, getApiOptions(Cart));
-  Api.addCollection(Accounts, getApiOptions(Accounts));
   Api.addCollection(Shipping, getApiOptions(Shipping));
-  Api.addCollection(Inventory, getApiOptions(Inventory));
-}
+};
